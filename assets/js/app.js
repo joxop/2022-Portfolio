e28bb1a889692736f55d42d1e5cfff0812e3f897
Jhevon Tulloch <Mrjtullo@gmail.com>
2022-06-14 15:10:40 -0400

    dark theme completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,34 @@ const toggleMenu = () => {
 }
 
 menuToggleButton.addEventListener('click', toggleMenu);
+
+const removeActiveLinkClass = e => {
+    if (e.target.classList.contains('list-link')){
+        menuElement.classList.remove('active');
+        menuToggleButton.classList.remove('active');
+    }
+}
+
+document.addEventListener('click', removeActiveLinkClass);
+
+// toggle theme store selection in local storage
+const themeToggleButton = document.querySelector('.theme-toggle-button')
+const bodyElement = document.body;
+const currentTheme = localStorage.getItem('darkTheme');
+
+if(currentTheme){
+    bodyElement.classList.add('dark-theme')
+}
+
+const toggleTheme = () => {
+    bodyElement.classList.toggle('dark-theme')
+   
+    if (bodyElement.classList.contains('dark-theme')){
+        localStorage.setItem('darkTheme', 'active')
+    }else{
+        localStorage.removeItem('darkTheme')
+
+    }
+};
+
+themeToggleButton.addEventListener('click', toggleTheme)
