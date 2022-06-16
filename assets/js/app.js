0a4da507cf2f649b31e1ce422ae8d04d9f8272b3
Jhevon Tulloch <Mrjtullo@gmail.com>
2022-06-15 23:23:49 -0400

    about me section completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,39 @@
-
-
-
-const menuToggleButton = document.querySelector('.menu-toggle-button');
-const menuElement = document.querySelector('.menu')
+const menuToggleButton = document.querySelector(".menu-toggle-button");
+const menuElement = document.querySelector(".menu");
 
 const toggleMenu = () => {
-    menuElement.classList.toggle('active');
-    menuToggleButton.classList.toggle('active');
+  menuElement.classList.toggle("active");
+  menuToggleButton.classList.toggle("active");
+};
 
-}
+menuToggleButton.addEventListener("click", toggleMenu);
 
-menuToggleButton.addEventListener('click', toggleMenu);
+const removeActiveLinkClass = (e) => {
+  if (e.target.classList.contains("list-link")) {
+    menuElement.classList.remove("active");
+    menuToggleButton.classList.remove("active");
+  }
+};
 
-const removeActiveLinkClass = e => {
-    if (e.target.classList.contains('list-link')){
-        menuElement.classList.remove('active');
-        menuToggleButton.classList.remove('active');
-    }
-}
-
-document.addEventListener('click', removeActiveLinkClass);
+document.addEventListener("click", removeActiveLinkClass);
 
 // toggle theme store selection in local storage
-const themeToggleButton = document.querySelector('.theme-toggle-button')
+const themeToggleButton = document.querySelector(".theme-toggle-button");
 const bodyElement = document.body;
-const currentTheme = localStorage.getItem('darkTheme');
+const currentTheme = localStorage.getItem("darkTheme");
 
-if(currentTheme){
-    bodyElement.classList.add('dark-theme')
+if (currentTheme) {
+  bodyElement.classList.add("dark-theme");
 }
 
 const toggleTheme = () => {
-    bodyElement.classList.toggle('dark-theme')
-   
-    if (bodyElement.classList.contains('dark-theme')){
-        localStorage.setItem('darkTheme','on')
-    }else{
-        localStorage.removeItem('darkTheme')
+  bodyElement.classList.toggle("dark-theme");
 
-    }
+  if (bodyElement.classList.contains("dark-theme")) {
+    localStorage.setItem("darkTheme", "on");
+  } else {
+    localStorage.removeItem("darkTheme");
+  }
 };
 
-themeToggleButton.addEventListener('click', toggleTheme)
+themeToggleButton.addEventListener("click", toggleTheme);
